1df4aa4fcf73155881e3373be201a2738ce8844e
Fiteropp <140279026+Fiteropp@users.noreply.github.com>
2025-02-27 12:07:00 +0200

    router fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, Route, Routes } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import Navbar from "./components/Navbar/Navbar";
+
+import Homepage from "./pages/Homepage.jsx";
+import Projects from "./pages/Projects.jsx";
+import Contact from "./pages/ContactMe.jsx";
 
 const App = () => {
   const location = useLocation();
@@ -13,10 +17,17 @@ const App = () => {
           key={location.pathname} // Ensures Framer Motion detects route changes
           initial={{ filter: "blur(20px)", opacity: 0 }}
           animate={{ filter: "blur(0px)", opacity: 1 }}
-          
+
           transition={{ duration: 0.4, ease: "easeOut" }}
         >
-          <Outlet />
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route index={true} path="/" element={<Homepage />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="contact-me" element={<Contact />} />
+          </Routes>
+
+
         </motion.div>
       </AnimatePresence>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,22 @@
 import {React, StrictMode} from "react";
 import { createRoot } from "react-dom/client";
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
   HashRouter
 } from "react-router-dom";
 
 
 
 
-import Homepage from "./pages/Homepage.jsx";
-import Projects from "./pages/Projects.jsx";
-import Contact from "./pages/ContactMe.jsx";
+
 
 import App from "./App.jsx";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<Homepage />} />
-      <Route path="projects" element={<Projects />} />
-      <Route path="contact-me" element={<Contact />} />
-    </Route>
-      
-  )
-);
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HashRouter>
-      <RouterProvider router={router} />
+      <App />
     </HashRouter> 
   </StrictMode>
 );
